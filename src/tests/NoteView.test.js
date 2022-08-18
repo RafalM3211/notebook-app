e2d2ad66f646378b2025f215dd86b033fb79ae88
Rafal Malinowski <cyrax1321@gmail.com>
2022-08-18 19:11:06 +0200

    add state wrapper to test utils

--- src/tests/NoteView.test.js
+++ src/tests/NoteView.test.js
@@ -1,39 +1,24 @@
 import * as ReactRouter from 'react-router';
 import NoteView from "../components/NoteView/NoteView";
 import {render, screen, getByText} from "@testing-library/react";
-import {wrappedRender} from "./testUtils";
+import {wrappedRender, wrapWithAppState} from "./testUtils";
+import React, { Children, useState } from 'react';
+
+
+
+
 
 it("displays data correct", ()=>{
     //arrange
-    const setEditedNote=jest.fn();
-    const editedNote=jest.fn();
-    const setFavouriteShortened=jest.fn();
-    const notes=[{
-        "id": 1,
-        "title": "test1",
-        "content": "test1 content here",
-        "isFavourite": false,
-        "description": "description test"
-      },
-      {
-        "id": 2,
-        "title": "example",
-        "content": "exaple example",
-        "isFavourite": false,
-        "description": "example note"
-    }];
+
+    const StateWrappedNoteView=wrapWithAppState(NoteView);
 
     //act
-
     jest.spyOn(ReactRouter, 'useParams').mockReturnValue({"*": 1});
-
     wrappedRender(
-        <NoteView 
-         setEditedNote={setEditedNote}
-         editedNote={editedNote}
-         setFavourite={setFavouriteShortened}
-         notes={notes}
-        />
+        <StateWrappedNoteView />
     )
+
     //assert
+    expect(screen.getByRole("textbox")).toHaveTextContent("test1 content here");
 })
--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -1,7 +1,37 @@
 import {MemoryRouter} from "react-router-dom";
-import {render} from "@testing-library/react"
+import {render} from "@testing-library/react";
+import { useState } from "react";
 
-function renderWrapper(props){
+export function wrapWithAppState(Comp){
+    return function(props){
+        const [editedNote, setEditedNote]=useState({});
+        const setFavouriteShortened=jest.fn();
+        const notes=[{
+            "id": 1,
+            "title": "test1",
+            "content": "test1 content here",
+            "isFavourite": true,
+            "description": "description test"
+        },
+        {
+            "id": 2,
+            "title": "example",
+            "content": "exaple example",
+            "isFavourite": false,
+            "description": "example note"
+        }];
+
+        return <Comp
+                setEditedNote={setEditedNote}
+                editedNote={editedNote}
+                setFavourite={setFavouriteShortened}
+                notes={notes}
+                {...props}
+             />
+    }
+}
+
+function RenderWrapper(props){
     return(
         <MemoryRouter>
             {props.children}
@@ -10,6 +40,6 @@ function renderWrapper(props){
 }
 
 export function wrappedRender(element, options){
-    render(element, {wrapper: renderWrapper, ...options});
+    render(element, {wrapper: RenderWrapper, ...options});
 }
 
