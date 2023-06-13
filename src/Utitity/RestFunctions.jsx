e5481cac306509cb286167f026507bf791141d3b
Rafal Malinowski <cyrax1321@gmail.com>
2023-06-13 22:17:31 +0200

    make notes deleting and setting favourite using localstorage

--- src/Utitity/RestFunctions.jsx
+++ src/Utitity/RestFunctions.jsx
@@ -2,42 +2,15 @@ import { getIndexById } from "./other";
 
 const URL="http://localhost:3000";
 
-const mockedNotes=[
-    {
-      "id": 1,
-      "title": "hello",
-      "content": "asfasf",
-      "isFavourite": false,
-      "description": "description test"
-    },
-    {
-      "id": 2,
-      "title": "bye",
-      "content": "asasffadfgf",
-      "isFavourite": false,
-      "description": "example note"
-    },
-    {
-      "id": 3,
-      "title": "third note",
-      "content": "asasdasd asd d",
-      "isFavourite": false,
-      "description": "description here"
-    },
-    {
-      "id": 4,
-      "title": "testchujsjd",
-      "content": "testchuj",
-      "isFavourite": "",
-      "description": "hghgvhjv"
-    }
-  ]
+function getNotesFromLocalstorage(){
+    const notes=JSON.parse(localStorage.getItem("notes"))||[];
+    return notes
+}
 
 async function getNotes(){
     try{
-        const notes=localStorage.getItem("notes");
-        //const notes=await fetch(`${URL}/notes`).then(res=>res.json());
-        return JSON.parse(notes)||[]
+        const notes=getNotesFromLocalstorage();
+        return notes
     }
     catch(err){
         console.error(err);
@@ -47,9 +20,12 @@ async function getNotes(){
 
 async function sendDeletion(id){
     try{
-        /* await fetch(`${URL}/notes/${id}`,{
-            method: "DELETE"
-        }) */
+        const notes=getNotesFromLocalstorage();
+        const noteIndex=getIndexById(id, notes);
+        if(noteIndex){
+            notes.splice(noteIndex, 1);
+            localStorage.setItem("notes", JSON.stringify(notes));
+        }        
     }
     catch(err){
         console.error(err);
@@ -60,7 +36,7 @@ async function sendDeletion(id){
 async function sendNote(note, id, isNoteNew){
     const protocol=isNoteNew? "POST":"PUT";
     try{
-        const notes=JSON.parse(localStorage.getItem("notes"))||[];
+        const notes=getNotesFromLocalstorage();
         const sendedNote={id, ...note};
         const noteIndex=getIndexById(id, notes);
         if(!noteIndex){
@@ -69,15 +45,8 @@ async function sendNote(note, id, isNoteNew){
         else{
             notes[noteIndex]=sendedNote;
         }
-        
         localStorage.setItem("notes", JSON.stringify(notes));
-        /* await fetch(`${URL}/notes/${isNoteNew?"":id}`, {
-            method: protocol,
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: note
-        }) */
+
     }
     catch(err){
         console.error(err);
@@ -87,16 +56,12 @@ async function sendNote(note, id, isNoteNew){
 
 async function patchFavourite(id, value){
     try{
-         const dataToSend=JSON.stringify({
-            isFavourite: value
-        })
-        /*await fetch(`${URL}/notes/${id}`,{
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: dataToSend
-        }); */
+        const notes=getNotesFromLocalstorage();
+        const noteIndex=getIndexById(id, notes);
+        if(noteIndex){
+            notes[noteIndex].isFavourite=value;
+            localStorage.setItem("notes", JSON.stringify(notes));
+        }  
     }
     catch(err){
         console.error(err);
